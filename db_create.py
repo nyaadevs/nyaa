b147cfeb81baa5e06d3995f9611bcacec9aafeb0
Yara Naika <a@b.c>
2017-05-13 23:51:29 -0700

    Ghetto migrate solution, in case you re-run db_create, it won't add categories again

--- db_create.py
+++ db_create.py
@@ -5,33 +5,34 @@ from nyaa import app, db, models
 
 db.create_all()
 
-# Insert categories
+# Insert categories and insert if it doesn't eixst
+existing_cats = models.MainCategory.query.all()
+if not existing_cats:
+    if app.config['SITE_FLAVOR'] == 'nyaa':
+        CATEGORIES = [
+            ('Anime', ['Anime Music Video', 'English-translated', 'Non-English-translated', 'Raw']),
+            ('Audio', ['Lossless', 'Lossy']),
+            ('Literature', ['English-translated', 'Non-English-translated', 'Raw']),
+            ('Live Action', ['English-translated', 'Idol/Promotional Video', 'Non-English-translated', 'Raw']),
+            ('Pictures', ['Graphics', 'Photos']),
+            ('Software', ['Applications', 'Games']),
+        ]
+    elif app.config['SITE_FLAVOR'] == 'sukebei':
+        CATEGORIES = [
+            ('Art', ['Anime', 'Doujinshi', 'Games', 'Manga', 'Pictures']),
+            ('Real Life', ['Photobooks / Pictures', 'Videos']),
+        ]
+    else:
+        CATEGORIES = []
 
-if app.config['SITE_FLAVOR'] == 'nyaa':
-    CATEGORIES = [
-        ('Anime', ['Anime Music Video', 'English-translated', 'Non-English-translated', 'Raw']),
-        ('Audio', ['Lossless', 'Lossy']),
-        ('Literature', ['English-translated', 'Non-English-translated', 'Raw']),
-        ('Live Action', ['English-translated', 'Idol/Promotional Video', 'Non-English-translated', 'Raw']),
-        ('Pictures', ['Graphics', 'Photos']),
-        ('Software', ['Applications', 'Games']),
-    ]
-elif app.config['SITE_FLAVOR'] == 'sukebei':
-    CATEGORIES = [
-        ('Art', ['Anime', 'Doujinshi', 'Games', 'Manga', 'Pictures']),
-        ('Real Life', ['Photobooks / Pictures', 'Videos']),
-    ]
-else:
-    CATEGORIES = []
+    for main_cat_name, sub_cat_names in CATEGORIES:
+        main_cat = models.MainCategory(name=main_cat_name)
+        for i, sub_cat_name in enumerate(sub_cat_names):
+            # Composite keys can't autoincrement, set sub_cat id manually (1-index)
+            sub_cat = models.SubCategory(id=i+1, name=sub_cat_name, main_category=main_cat)
+        db.session.add(main_cat)
 
-for main_cat_name, sub_cat_names in CATEGORIES:
-    main_cat = models.MainCategory(name=main_cat_name)
-    for i, sub_cat_name in enumerate(sub_cat_names):
-        # Composite keys can't autoincrement, set sub_cat id manually (1-index)
-        sub_cat = models.SubCategory(id=i+1, name=sub_cat_name, main_category=main_cat)
-    db.session.add(main_cat)
-
-db.session.commit()
+    db.session.commit()
 
 # Create fulltext index
 
