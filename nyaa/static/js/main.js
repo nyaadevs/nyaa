df16a60e2205a6befd93960526f2025640f8e127
Ricardo Ribeiro <ricardoribeiro_1991@hotmail.com>
2017-05-18 18:59:34 +0100

    Add site flavor and theme classes to body

--- nyaa/static/js/main.js
+++ nyaa/static/js/main.js
@@ -177,3 +177,21 @@ document.addEventListener("DOMContentLoaded", function() {
 // 		localStorage.setItem('theme', 'light');
 // 	}
 // }
+function addThemeClass(){
+	"dark" === localStorage.getItem("theme")?document.body.classList.add('dark'):document.body.classList.add('light');
+}
+
+function replaceThemeClass(){
+	if ("undefined" != typeof document.body) {
+		if (localStorage.getItem('theme') === 'dark') {
+			document.body.classList.remove('light');
+			document.body.classList.add('dark');
+		}
+		else{
+			document.body.classList.remove('dark');
+			document.body.classList.add('light');
+		}
+	}
+}
+
+window.onload=addThemeClass;
