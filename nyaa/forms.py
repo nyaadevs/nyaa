d89f74893ba48a8a3415ab7d3a4a3273031df917
TheAMM <the.actual.amm@gmail.com>
2017-05-26 23:27:30 +0300

    Remove username validator from login form to allow logging in with email
    Since our previous regex was broken, the validator passed practically everything.

--- nyaa/forms.py
+++ nyaa/forms.py
@@ -42,7 +42,7 @@ _username_validator = Regexp(
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username or email address', [DataRequired(), _username_validator])
+    username = StringField('Username or email address', [DataRequired()])
     password = PasswordField('Password', [DataRequired()])
 
 
