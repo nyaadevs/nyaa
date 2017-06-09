07374faba7f8eff98a2fa7be6d2f0df6e2aa81f6
Kfir Hadas <sharkykh@gmail.com>
2017-06-09 17:23:32 +0300

    Test timesince()

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -4,7 +4,7 @@ import datetime
 from email.utils import formatdate
 
 from nyaa.routes import (_jinja2_filter_rfc822, _jinja2_filter_rfc822_es, get_utc_timestamp,
-                         get_display_time)
+                         get_display_time, timesince)
 
 
 class TestFilters(unittest.TestCase):
@@ -42,9 +42,17 @@ class TestFilters(unittest.TestCase):
         test_date_str = '2017-02-15T11:15:34'
         self.assertEqual(get_display_time(test_date_str), '2017-02-15 11:15')
 
-    @unittest.skip('Not yet implemented')
     def test_timesince(self):
-        pass
+        now = datetime.datetime.utcnow()
+        self.assertEqual(timesince(now), 'just now')
+        self.assertEqual(timesince(now - datetime.timedelta(seconds=5)), '5 seconds ago')
+        self.assertEqual(timesince(now - datetime.timedelta(minutes=1)), '1 minute ago')
+        self.assertEqual(
+            timesince(now - datetime.timedelta(minutes=38, seconds=43)), '38 minutes ago')
+        self.assertEqual(
+            timesince(now - datetime.timedelta(hours=2, minutes=38, seconds=51)), '2 hours ago')
+        bigger = now - datetime.timedelta(days=3)
+        self.assertEqual(timesince(bigger), bigger.strftime('%Y-%m-%d %H:%M UTC'))
 
     @unittest.skip('Not yet implemented')
     def test_static_cachebuster(self):
