2507d7b0ace15da320faca4b0cd05c639bb3dad3
Kfir Hadas <sharkykh@gmail.com>
2017-07-15 02:21:44 +0300

    Refactor `nyaa/__init__.py` into an app factory

--- nyaa/__init__.py
+++ nyaa/__init__.py
@@ -9,71 +9,76 @@ from nyaa.extensions import assets, db, fix_paginate, toolbar
 from nyaa.template_utils import bp as template_utils_bp
 from nyaa.views import register_views
 
-app = flask.Flask(__name__)
-app.config.from_object('config')
 
-# Don't refresh cookie each request
-app.config['SESSION_REFRESH_EACH_REQUEST'] = False
+def create_app(config):
+    """ Nyaa app factory """
+    app = flask.Flask(__name__)
+    app.config.from_object(config)
 
-# Debugging
-if app.config['DEBUG']:
-    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-    toolbar.init_app(app)
-    app.logger.setLevel(logging.DEBUG)
+    # Don't refresh cookie each request
+    app.config['SESSION_REFRESH_EACH_REQUEST'] = False
 
-    # Forbid caching
-    @app.after_request
-    def forbid_cache(request):
-        request.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate, max-age=0'
-        request.headers['Pragma'] = 'no-cache'
-        request.headers['Expires'] = '0'
-        return request
+    # Debugging
+    if app.config['DEBUG']:
+        app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+        toolbar.init_app(app)
+        app.logger.setLevel(logging.DEBUG)
 
-else:
-    app.logger.setLevel(logging.WARNING)
+        # Forbid caching
+        @app.after_request
+        def forbid_cache(request):
+            request.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate, max-age=0'
+            request.headers['Pragma'] = 'no-cache'
+            request.headers['Expires'] = '0'
+            return request
 
-# Logging
-if 'LOG_FILE' in app.config:
-    from logging.handlers import RotatingFileHandler
-    app.log_handler = RotatingFileHandler(
-        app.config['LOG_FILE'], maxBytes=10000, backupCount=1)
-    app.logger.addHandler(app.log_handler)
+    else:
+        app.logger.setLevel(logging.WARNING)
 
-# Log errors and display a message to the user in production mdode
-if not app.config['DEBUG']:
-    @app.errorhandler(500)
-    def internal_error(exception):
-        app.logger.error(exception)
-        flask.flash(flask.Markup(
-            '<strong>An error occurred!</strong> Debug information has been logged.'), 'danger')
-        return flask.redirect('/')
+    # Logging
+    if 'LOG_FILE' in app.config:
+        from logging.handlers import RotatingFileHandler
+        app.log_handler = RotatingFileHandler(
+            app.config['LOG_FILE'], maxBytes=10000, backupCount=1)
+        app.logger.addHandler(app.log_handler)
 
-# Get git commit hash
-app.config['COMMIT_HASH'] = None
-master_head = os.path.abspath(os.path.join(os.path.dirname(__file__), '../.git/refs/heads/master'))
-if os.path.isfile(master_head):
-    with open(master_head, 'r') as head:
-        app.config['COMMIT_HASH'] = head.readline().strip()
+    # Log errors and display a message to the user in production mdode
+    if not app.config['DEBUG']:
+        @app.errorhandler(500)
+        def internal_error(exception):
+            app.logger.error(exception)
+            flask.flash(flask.Markup(
+                '<strong>An error occurred!</strong> Debug information has been logged.'), 'danger')
+            return flask.redirect('/')
 
-# Enable the jinja2 do extension.
-app.jinja_env.add_extension('jinja2.ext.do')
-app.jinja_env.lstrip_blocks = True
-app.jinja_env.trim_blocks = True
+    # Get git commit hash
+    app.config['COMMIT_HASH'] = None
+    master_head = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                               '../.git/refs/heads/master'))
+    if os.path.isfile(master_head):
+        with open(master_head, 'r') as head:
+            app.config['COMMIT_HASH'] = head.readline().strip()
 
-# Database
-fix_paginate()  # This has to be before the database is initialized
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['MYSQL_DATABASE_CHARSET'] = 'utf8mb4'
-db.init_app(app)
+    # Enable the jinja2 do extension.
+    app.jinja_env.add_extension('jinja2.ext.do')
+    app.jinja_env.lstrip_blocks = True
+    app.jinja_env.trim_blocks = True
 
-assets.init_app(app)
+    # Database
+    fix_paginate()  # This has to be before the database is initialized
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['MYSQL_DATABASE_CHARSET'] = 'utf8mb4'
+    db.init_app(app)
 
-# css = Bundle('style.scss', filters='libsass',
-#             output='style.css', depends='**/*.scss')
-# assets.register('style_all', css)
+    # Assets
+    assets.init_app(app)
+    # css = Bundle('style.scss', filters='libsass',
+    #             output='style.css', depends='**/*.scss')
+    # assets.register('style_all', css)
 
-with app.app_context():  # Temporary
-    # Register blueprints
+    # Blueprints
     app.register_blueprint(template_utils_bp)
     app.register_blueprint(api_blueprint, url_prefix='/api')
     register_views(app)
+
+    return app
--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
-from nyaa import app
+from nyaa import create_app
+
+app = create_app('config')
 app.run(host='0.0.0.0', port=5500, debug=True)
