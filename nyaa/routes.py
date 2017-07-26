2424639bf98951ab659e6109cf1af59eabea856e
Kfir Hadas <sharkykh@gmail.com>
2017-07-26 18:11:00 +0300

    Refactor routes.py

--- nyaa/routes.py
+++ nyaa/routes.py
@@ -1,25 +1,9 @@
-from nyaa import api_handler, app, template_utils, views
+from nyaa import app, template_utils, views
+from nyaa.api_handler import api_blueprint
 
-DEBUG_API = False
-
-
-# #################################### BLUEPRINTS ####################################
-
-def register_blueprints(flask_app):
-    """ Register the blueprints using the flask_app object """
-
-    # Template filters and globals
-    flask_app.register_blueprint(template_utils.bp)
-    # API routes
-    flask_app.register_blueprint(api_handler.api_blueprint, url_prefix='/api')
-    # Site routes
-    flask_app.register_blueprint(views.account_bp)
-    flask_app.register_blueprint(views.admin_bp)
-    flask_app.register_blueprint(views.main_bp)
-    flask_app.register_blueprint(views.site_bp)
-    flask_app.register_blueprint(views.torrents_bp)
-    flask_app.register_blueprint(views.users_bp)
-
-
-# When done, this can be moved to nyaa/__init__.py instead of importing this file
-register_blueprints(app)
+# Register all template filters and template globals
+app.register_blueprint(template_utils.bp)
+# Register the API routes
+app.register_blueprint(api_blueprint, url_prefix='/api')
+# Register the site's routes
+views.register(app)
