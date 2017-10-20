c54d9300e243be64d4044ae1e408fd45852cf04f
Nicolas F <ovdev@fratti.ch>
2017-10-20 13:21:31 +0200

    Fix filter dropdowns if JS is disabled
    Before bootstrap-select is loaded, a small JS piece replaces the
    class for the pickers with the appropriate bootstrap-select classes.
    
    If there is no JS, the dropdowns will stay as form-control.

---
+++ nyaa/static/js/compat.js
@@ -0,0 +1,5 @@
+// Filter dropdown styling if JS is enabled
+$(function() {
+	$("#navFilter-category, #navFilter-criteria").children("select").removeClass("form-control").addClass("selectpicker show-tick");
+	}
+);
