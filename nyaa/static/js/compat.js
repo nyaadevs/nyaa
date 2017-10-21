ca7dc276e22ca41e917b697bd7e2ed57d3098130
Arylide <Arylide@users.noreply.github.com>
2017-10-20 22:21:06 -0700

    Fix some text and revert a js thing

--- nyaa/static/js/compat.js
+++
@@ -1,5 +0,0 @@
-// Filter dropdown styling if JS is enabled
-$(function() {
-	$("#navFilter-category, #navFilter-criteria").children("select").removeClass("form-control").addClass("selectpicker show-tick");
-	}
-);
