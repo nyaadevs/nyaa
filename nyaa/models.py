cadc47fd3ded19a4afb68200c2b8a476f0139a13
Nicolas F <ovdev@fratti.ch>
2018-04-03 15:54:16 +0200

    Pregenerate Gravatar default image URLs
    If possible (i.e. SERVER_NAME is set), we can pregenerate the constant
    gravatar default URL once at application startup, and re-use that,
    as url_for calls are surprisingly expensive.
    
    Especially on torrent view pages with lots of comments, this cuts down
    on url_for calls massively, saving on my system about 0.3 ms per call.

--- nyaa/models.py
+++ nyaa/models.py
@@ -517,13 +517,18 @@ class User(db.Model):
         return all(checks)
 
     def gravatar_url(self):
+        if 'DEFAULT_GRAVATAR_URL' in app.config:
+            default_url = app.config['DEFAULT_GRAVATAR_URL']
+        else:
+            default_url = flask.url_for('static', filename='img/avatar/default.png',
+                                        _external=True)
         if app.config['ENABLE_GRAVATAR']:
             # from http://en.gravatar.com/site/implement/images/python/
             params = {
                 # Image size (https://en.gravatar.com/site/implement/images/#size)
                 's': 120,
                 # Default image (https://en.gravatar.com/site/implement/images/#default-image)
-                'd': flask.url_for('static', filename='img/avatar/default.png', _external=True),
+                'd': default_url,
                 # Image rating (https://en.gravatar.com/site/implement/images/#rating)
                 # Nyaa: PG-rated, Sukebei: X-rated
                 'r': 'pg' if app.config['SITE_FLAVOR'] == 'nyaa' else 'x',
@@ -532,7 +537,7 @@ class User(db.Model):
             return 'https://www.gravatar.com/avatar/{}?{}'.format(
                 md5(self.email.encode('utf-8').lower()).hexdigest(), urlencode(params))
         else:
-            return flask.url_for('static', filename='img/avatar/default.png')
+            return default_url
 
     @property
     def userlevel_str(self):
