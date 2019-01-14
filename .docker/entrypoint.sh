d31108747a6b0158310276986dbe3811925fcdbb
Alex Headley <aheadley@waysaboutstuff.com>
2019-01-14 12:36:44 -0500

    fix remaining issues and smooth out rough edges, docker compose fully working now

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -5,41 +5,46 @@
 pushd /nyaa
 
 echo 'Waiting for MySQL to start up'
-while ! echo HELO | nc mariadb 3306; do
-	echo -n '.'
+while ! echo HELO | nc mariadb 3306 &>/dev/null; do
 	sleep 1
 done
 echo 'DONE'
 
-if ! [ -f /tmp/flag-db_create ]; then
+if ! [ -f /elasticsearch-sync/flag-db_create ]; then
 	python3 ./db_create.py
-	touch /tmp/flag-db_create
+	touch /elasticsearch-sync/flag-db_create
 fi
 
-if ! [ -f /tmp/flag-db_migrate ]; then
+if ! [ -f /elasticsearch-sync/flag-db_migrate ]; then
 	python3 ./db_migrate.py stamp head
-	touch /tmp/flag-db_migrate
+	touch /elasticsearch-sync/flag-db_migrate
 fi
 
 echo 'Waiting for ES to start up'
-while ! echo HELO | nc elasticsearch 9200; do
-	echo -n '.'
+while ! echo HELO | nc elasticsearch 9200 &>/dev/null; do
 	sleep 1
 done
 echo 'DONE'
 
-if ! [ -f /tmp/flag-create_es ]; then
+echo 'Waiting for ES to be ready'
+while ! curl -s -XGET 'elasticsearch:9200/_cluster/health?pretty=true&wait_for_status=green' &>/dev/null; do
+	sleep 1
+done
+echo 'DONE'
+
+if ! [ -f /elasticsearch-sync/flag-create_es ]; then
 	# @source create_es.sh
 	# create indices named "nyaa" and "sukebei", these are hardcoded
 	curl -v -XPUT 'elasticsearch:9200/nyaa?pretty' -H"Content-Type: application/yaml" --data-binary @es_mapping.yml
 	curl -v -XPUT 'elasticsearch:9200/sukebei?pretty' -H"Content-Type: application/yaml" --data-binary @es_mapping.yml
-	touch /tmp/flag-create_es
+	touch /elasticsearch-sync/flag-create_es
 fi
 
-if ! [ -f /tmp/flag-import_to_es ]; then
-	python3 ./import_to_es.py
-	touch /tmp/flag-import_to_es
+if ! [ -f /elasticsearch-sync/flag-import_to_es ]; then
+	python3 ./import_to_es.py | tee /elasticsearch-sync/import.out
+	grep -A1 'Save the following' /elasticsearch-sync/import.out | tail -1 > /elasticsearch-sync/pos.json
+	touch /elasticsearch-sync/flag-import_to_es
 fi
 
 echo 'Starting the Flask app'
-python3 WSGI.py
+/usr/local/bin/uwsgi /nyaa/.docker/uwsgi.config.ini
