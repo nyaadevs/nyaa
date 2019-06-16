b27686ccab62bfa2494cc6527c9f1504ce9bbdbc
Nicolas F <ovdev@fratti.ch>
2019-06-16 23:08:23 +0200

    Add login endpoint rate limiting
    This doesn't discriminate between failed logins and successful
    logins, but only counts POST requests. The limit is set to 6 per
    hour.

--- nyaa/extensions.py
+++ nyaa/extensions.py
@@ -5,12 +5,15 @@ from flask.config import Config
 from flask_assets import Environment
 from flask_caching import Cache
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from flask_sqlalchemy import BaseQuery, Pagination, SQLAlchemy
 
 assets = Environment()
 db = SQLAlchemy()
 toolbar = DebugToolbarExtension()
 cache = Cache()
+limiter = Limiter(key_func=get_remote_address)
 
 
 class LimitedPagination(Pagination):
