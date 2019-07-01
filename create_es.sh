b797f45f2880c438a8ffe99b751b55962fd19f9f
queue <queue@hakase.org>
2019-06-30 20:20:24 -0600

    es_mapping.yml: fix deprecated mapping type
    https://www.elastic.co/guide/en/elasticsearch/reference/6.7/removal-of-types.html#_schedule_for_removal_of_mapping_types
    
    it gives a really unhelpful error otherwise, oof.

--- create_es.sh
+++ create_es.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 # create indices named "nyaa" and "sukebei", these are hardcoded
 curl -v -XPUT 'localhost:9200/nyaa?pretty' -H"Content-Type: application/yaml" --data-binary @es_mapping.yml
